58bf4716f48ea97790c0187e609302c46f8992f7
Jeremy Chen <jeremychen0127@gmail.com>
2019-03-19 13:55:10 -0400

    Add Python scripts for extracting/converting dataset

---
+++ preprocessing/convert_to_graph.py
@@ -0,0 +1,13 @@
+with open('./citation-acm-cleaned.txt', 'r') as fp:
+    index = ''
+    dest_vertices = []
+    for line in fp:
+        if line.startswith("#index"):
+            if index != '':
+                print index + " " + " ".join(dest_vertices)
+                dest_vertices = []
+            index = line.split("#index")[1].rstrip()
+        elif line.startswith("#%"):
+            dest_vertices.append(line.split("#%")[1].rstrip())
+ 
+    print index + " " + " ".join(dest_vertices)
---
+++ preprocessing/extract_abstract.py
@@ -0,0 +1,18 @@
+with open('./citation-acm-cleaned.txt', 'r') as fp:
+    index = ''
+    abstract_start = False
+    for line in fp:
+        if line.startswith("#!"):
+            abstract_start = True
+            abstract = line.split("#!")[1].rstrip() + " "
+        elif abstract_start:
+            if line.startswith("#"):
+                abstract_start = False
+                print index + "###" + abstract
+            else:
+                abstract += line.rstrip() + " "
+        elif line.startswith("#index"):
+            index = line.split("#index")[1].rstrip()
+    
+    if abstract_start:
+        print index + "###" + abstract
